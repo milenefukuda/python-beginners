e9b40cab0a2fdb61c4940e2c2d4d999ba3125f5a
Milene Fukuda <milene.fukuda@gmail.com>
2023-04-13 19:16:45 +0100

    functions

---
+++ functions.py
@@ -0,0 +1,27 @@
+# argumento são os dados
+# parametros são placeholders
+
+def hello_world():
+    print("Hello world!")
+
+hello_world()
+
+def sum(num1=0, num2=0):
+    if(type(num1) is not int or type(num2) is not int):
+        return
+    return num1 + num2
+
+total = sum(7, 2)
+print(total)
+
+def multiple_items(*args):
+    print(args)
+    print(type(args))
+
+multiple_items("Dave", "John", "Sara")
+
+def mult_named_items(**kwargs):
+    print(kwargs)
+    print(type(kwargs))
+
+mult_named_items(first="Milene", last = "Fukuda")
